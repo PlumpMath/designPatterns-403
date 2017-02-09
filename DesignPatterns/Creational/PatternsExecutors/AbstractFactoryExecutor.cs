d58b4bb723f29b7a506ea29117eb1981aa287b1d
Tomasz Kaczmarek <t.kaczmarek@wp.pl>
2017-02-09 12:26:25 +0100

    abstractFactory executor

---
+++ DesignPatterns/Creational/PatternsExecutors/AbstractFactoryExecutor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.PatternsExecutors
+{
+    public class AbstractFactoryExecutor : IPatternExecutor
+    {
+        public string Description()
+        {
+            return string.Format("Provide an interface for creating families of related {0}or dependent objects without specifying their concrete classes. ", Environment.NewLine);
+        }
+
+        public void Execute()
+        {
+            
+        }
+
+        public void Message(string message)
+        {
+            
+        }
+    }
+}
