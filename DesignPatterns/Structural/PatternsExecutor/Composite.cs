176105bda0eb5d38d9aab4cad1a8d765d015dfb7
Tomasz Kaczmarek <t.kaczmarek@wp.pl>
2017-02-10 17:09:11 +0100

    Composite part1

---
+++ DesignPatterns/Structural/PatternsExecutor/Composite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.PatternsExecutor
+{
+    public class Composite : IPatternExecutor
+    {
+        public string Description()
+        {
+            return string.Format("Compose objects into tree structures to represent part-whole hierarchies. {0}Composite lets clients treat individual objects and compositions of objects uniformly. ", Environment.NewLine);
+        }
+
+        public void Execute()
+        {
+            
+        }
+    }
+}
