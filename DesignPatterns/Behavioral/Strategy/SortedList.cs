0a0f19912bd062d7171bba6b9997224251c95d70
Tomasz Kaczmarek <t.kaczmarek@wp.pl>
2017-02-17 10:35:29 +0100

    Strategy pattern

---
+++ DesignPatterns/Behavioral/Strategy/SortedList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Strategy
+{
+    public class SortedList
+    {
+        private SortStrategy _strategy;
+        private List<string> _list = new List<string>();
+
+        public void SetSortStrategy(SortStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void Add(string name)
+        {
+            _list.Add(name);
+        }
+
+        public void Sort()
+        {
+            _strategy.Sort(_list);
+
+            foreach(var l in _list)
+            {
+                Console.WriteLine(l);
+            }
+        }
+
+    }
+}
---
+++ DesignPatterns/Patterns/PatternExecutor/Behavioral/Strategy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DesignPatterns.Common.Base;
+using DesignPatterns.Behavioral.Strategy;
+
+namespace DesignPatterns.Patterns.PatternExecutor.Behavioral
+{
+    public class Strategy : IPatternExecutor
+    {
+        public string Description()
+        {
+            return string.Format("Define a family of algorithms, encapsulate each one, and make them interchangeable. {0}Strategy lets the algorithm vary independently from clients that use it. ", Environment.NewLine);
+        }
+
+        public string DescriptionPL()
+        {
+            return string.Format("Czynnościowy wzorzec projektowy, który definiuje rodzinę wymiennych algorytmów i {0}kapsułkuje je w postaci klas. Umożliwia wymienne stosowanie każdego z nich {0}w trakcie działania aplikacji niezależnie od korzystających z nich klientów.", Environment.NewLine);
+        }
+
+        public void Execute()
+        {
+            SortedList list = new SortedList();
+
+            list.Add("Tomek");
+            list.Add("Janek");
+            list.Add("Marek");
+
+            list.SetSortStrategy(new QuickSort());
+
+            list.Sort();
+
+            list.SetSortStrategy(new MergeSort());
+
+            list.Sort();
+
+            list.SetSortStrategy(new ShellSort());
+
+            list.Sort();
+        }
+    }
+}
