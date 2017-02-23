f4a444a01eabe5cf30c5003a4a1040ee55218ccf
Tomasz Kaczmarek <t.kaczmarek@wp.pl>
2017-02-23 11:42:14 +0100

    State pattern executor

---
+++ DesignPatterns/Patterns/PatternExecutor/Behavioral/State.cs
@@ -0,0 +1,24 @@
+﻿
+using DesignPatterns.Common.Base;
+
+
+namespace DesignPatterns.Patterns.PatternExecutor.Behavioral
+{
+    public class State : IPatternExecutor
+    {
+        public string Description()
+        {
+            return string.Format("Allow an object to alter its behavior when its internal state changes. The object will appear to change its class. ");
+        }
+
+        public string DescriptionPL()
+        {
+            return string.Format("Wzorzec projektowy, który umożliwia zmianę zachowania obiektu poprzez zmianę jego stanu wewnętrznego. Innymi słowy – uzależnia sposób działania obiektu od stanu w jakim się aktualnie znajduje.");
+        }
+
+        public void Execute()
+        {
+            
+        }
+    }
+}
