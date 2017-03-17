276cb52943e116db40be164223cd6cbca22f45f0
Tomasz Kaczmarek <t.kaczmarek@wp.pl>
2017-03-17 15:16:47 +0100

    like jon skeet

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -31,16 +31,18 @@ namespace DesignPatterns
                 var cache = LocalCache.Instance.Get<IDictionary<int, string>>(MENU_CACHE_NAME);
 
                 if (cache != null)
+                {
                     Menu = cache;
+                }
                 else
                 {
                     Menu = menu.BuildMenu<PatternsTypeBase>();
                     LocalCache.Instance.Add<IDictionary<int, string>>(Menu, MENU_CACHE_NAME);
-                }
-              
+                }            
                 foreach (var item in Menu)
+                {
                     Console.WriteLine("[{0}]. {1}.", item.Key, item.Value);
-
+                }
                 Console.WriteLine("[E]. Exit");
 
                 choose = Console.ReadLine();
@@ -50,10 +52,11 @@ namespace DesignPatterns
                     SelectPattern(number);
                 }
                 else
+                {
                     Console.WriteLine("Incorect format");
+                }                  
             }
-            while (choose.ToUpper() != END);
-                
+            while (choose.ToUpper() != END);                
         }
               
         static void SelectPattern(int number)
